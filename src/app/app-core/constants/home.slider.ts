6fc519601c024964632c3499fcaa359bdacc48ba
Jamel Eid Yassin <42769414+jamelyassin84@users.noreply.github.com>
2023-01-18 11:06:25 +0800

    slider

---
+++ src/app/app-core/constants/home.slider.ts
@@ -0,0 +1,10 @@
+export const HOME_SLIDERS = [
+    '/assets/app/home/1.jpg',
+    '/assets/app/home/2.jpg',
+    '/assets/app/home/3.JPG',
+    '/assets/app/home/4.JPG',
+    '/assets/app/home/5.JPG',
+    '/assets/app/home/6.jpg',
+    '/assets/app/home/7.JPG',
+    '/assets/app/home/8.JPG',
+]
--- src/app/modules/landing/home/home-section1/home-section1.component.ts
+++ src/app/modules/landing/home/home-section1/home-section1.component.ts
@@ -1,8 +1,58 @@
 import {Component} from '@angular/core'
+import {dbwAnimations} from '@digital_brand_work/animations/animation.api'
+import {HOME_SLIDERS} from 'app/app-core/constants/home.slider'
 
 @Component({
     selector: 'home-section1',
     templateUrl: './home-section1.component.html',
     styleUrls: ['./home-section1.component.scss'],
+    animations: [...dbwAnimations],
 })
-export class HomeSection1Component {}
+export class HomeSection1Component {
+    constructor() {}
+
+    readonly HOME_SLIDERS = HOME_SLIDERS
+
+    currentSlide: string = HOME_SLIDERS[0]
+
+    slideInterval: any
+
+    ngOnInit(): void {
+        this.slideInterval = setInterval(() => {
+            this.slide()
+        }, 2000)
+    }
+
+    ngOnDestroy(): void {
+        clearInterval(this.slideInterval)
+    }
+
+    slide(direction: 'left' | 'right' = 'right') {
+        const photo = HOME_SLIDERS[0]
+
+        const nextIndex = this.HOME_SLIDERS.findIndex(
+            (slide) => slide === this.currentSlide,
+        )
+
+        if (direction === 'right') {
+            if (!this.HOME_SLIDERS[nextIndex + 1]) {
+                this.currentSlide = photo
+                return
+            }
+
+            this.currentSlide = this.HOME_SLIDERS[nextIndex + 1]
+            return
+        }
+
+        if (!this.HOME_SLIDERS[nextIndex - 1]) {
+            this.currentSlide = this.HOME_SLIDERS[this.HOME_SLIDERS.length - 1]
+            return
+        }
+
+        this.currentSlide = this.HOME_SLIDERS[nextIndex - 1]
+    }
+
+    trackByFn(index: number, item: any): any {
+        return item.id || index
+    }
+}
