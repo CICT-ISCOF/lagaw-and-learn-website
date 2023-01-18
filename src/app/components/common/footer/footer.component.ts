2917a5cd8c032ea65859ac4361b02c47a048afb1
Jamel Eid Yassin <42769414+jamelyassin84@users.noreply.github.com>
2023-01-18 13:44:45 +0800

    Changes

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
-  selector: 'footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+    redirect(url: string) {
+        location.href = url
+    }
 }
