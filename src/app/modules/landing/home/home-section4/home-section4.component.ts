a48402454cdefc9df2246492f05449296f402672
rhys2226 <edzel0718@gmail.com>
2023-03-12 10:48:28 +0800

    changes

--- src/app/modules/landing/home/home-section4/home-section4.component.ts
+++ src/app/modules/landing/home/home-section4/home-section4.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core'
+import { faqs } from 'app/app-core/constants/faq'
 
 @Component({
     selector: 'home-section4',
@@ -6,6 +7,8 @@ import {Component} from '@angular/core'
     styleUrls: ['./home-section4.component.scss'],
 })
 export class HomeSection4Component {
+    faqs = faqs
+    currentIndex=100
     trackByFn(index: number, item: any): any {
         return item.id || index
     }
