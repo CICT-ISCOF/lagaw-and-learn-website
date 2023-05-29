3f494f3b520b1fa7ba62bddd1a5408924bed3c59
rhys2226 <edzel0718@gmail.com>
2023-05-30 00:04:42 +0800

    changes

--- src/app/app-core/constants/project-components/fish-processing.ts
+++ src/app/app-core/constants/project-components/fish-processing.ts
@@ -23,7 +23,13 @@ export const FISH_PROCESSING = {
                 id: '2',
                 title: 'Stocking',
                 images: [
-                    'https://drive.google.com/uc?export=view&id=1MTDCvtlPGFDBj2kAtKKr9KYcM3MZGGyQ',
+                    'https://drive.google.com/uc?export=view&id=1ZG6LaBjpL8hHFrqsGVFkydf10IyT3FoQ',
+                    'https://drive.google.com/uc?export=view&id=1GJCSY7ZO5O5Cp3G0mVSa1mnfgeKgI4mH',
+                    'https://drive.google.com/uc?export=view&id=1glfUI2WVSr3hp55R8Dni1pj8M5yfbXIp',
+                    'https://drive.google.com/uc?export=view&id=15CrWFGZvt0sJDuB1YtrXmClCHbRKchmj',
+                    'https://drive.google.com/uc?export=view&id=1WbvwJHOpGg9oX2GvxVVTqa6WDsYH4NCB',
+                    'https://drive.google.com/uc?export=view&id=1jAR86SUz8bvt-xAO10Vh4FSsFTZji4D1',
+                    'https://drive.google.com/uc?export=view&id=1nVjsR6MqJ-2cGUNIc26aQe0uff9rff0A',
                 ],
                 description:
                     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
@@ -33,7 +39,14 @@ export const FISH_PROCESSING = {
                 id: '3',
                 title: 'Transfer of Stock ( from Nursery Pond to Trasition Pond',
                 images: [
-                    'https://drive.google.com/uc?export=view&id=1InE4P0MTS6u94MtAV4icZh51bAn8ABKp',
+                    'https://drive.google.com/uc?export=view&id=1obqjHubSQuspTCTY324uAobcDBTNdhde',
+                    'https://drive.google.com/uc?export=view&id=1sN2zK3lEBUsOAHGsB_FIZyTUKOWO8yx5',
+                    'https://drive.google.com/uc?export=view&id=1xkzoZCv8P8NEY13qKgQguZtwRulW-rYG',
+                    'https://drive.google.com/uc?export=view&id=1TZ1j4s4tUC9V7gjrLiIqLBOcU_lOWHOP',
+                    'https://drive.google.com/uc?export=view&id=1GHWALZYFzrhSs1c-ona5elMoIzSWMepn',
+                    'https://drive.google.com/uc?export=view&id=1hVKPRLWA2PNDt8ygTqrrrH-X-bahtLHE',
+                    'https://drive.google.com/uc?export=view&id=1KnOP6vxF8kEM9nh7BisimuPsyMIBN6vt',
+                    'https://drive.google.com/uc?export=view&id=1tkt0dqjM_9er-9n4mxz5lkQfLCxHX3O5',
                 ],
                 description:
                     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
