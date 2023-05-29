6fef4736cb4ffe434d01edebf674d8ff14bfcdb4
rhys2226 <edzel0718@gmail.com>
2023-05-29 20:57:49 +0800

    changes

--- src/app/app-core/constants/project-component.ts
+++ src/app/app-core/constants/project-component.ts
@@ -1,5 +1,6 @@
 import {ProjectComponent} from '../models/project-component.model'
 
+
 export const PROJECT_COMPONENTS: ProjectComponent[] = [
     {
         id: '1',
@@ -12,10 +13,51 @@ export const PROJECT_COMPONENTS: ProjectComponent[] = [
             //     'https://drive.google.com/uc?export=download&id=1Uz6R862hWGtPtTqM7gIt80IlWqJ_LhEA',
             items: [
                 {
-                    title: 'Pond Preparation',
+                    title: 'Boneless Bangus',
                     images: [
-                        'https://drive.google.com/uc?export=download&id=1g1mJxHfJ2_DQoseZe6pj6P9mqN4xOX4N',
-                        'https://drive.google.com/uc?export=download&id=1g1mJxHfJ2_DQoseZe6pj6P9mqN4xOX4N',
+                        'https://drive.google.com/uc?export=view&id=19h_vRLglnDjVksBeRGavveMkG6_WuDfQ',
+                        'https://drive.google.com/uc?export=view&id=1ZJGsM7bym-Uj_wvLfA0tCDadkgBJ0cHV',
+                        'https://drive.google.com/uc?export=view&id=1y708oJ7sB-e-zESZJBWctTtmiwnOj0qH',
+                        'https://drive.google.com/uc?export=view&id=1fsAMYFkl-e8_zloCuHRkGf9vjoL3j5Rl',
+                        'https://drive.google.com/uc?export=view&id=1kIGt_DqJIjAeGF52j6Wj2m8C6AG65IVl',
+                        'https://drive.google.com/uc?export=view&id=1TfjUU9AwRKnWhRNMcfmSZN3IpyQo0MEn',
+                        'https://drive.google.com/uc?export=view&id=1m97Ua0Ih0eGkT_OzLdQESH0OVV4TXpjc',
+                        
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+                {
+
+                    title: 'Boneless Kroepeck',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=1MTDCvtlPGFDBj2kAtKKr9KYcM3MZGGyQ',
+                        'https://drive.google.com/uc?export=view&id=18WGsGar0FMzpQ48ELA8P_hxOYADCGxWZ',
+                        'https://drive.google.com/uc?export=view&id=1ERyXUDMwMC91aWDlLQl_INmwHG57q57D',
+                        'https://drive.google.com/uc?export=view&id=1_A8Y4hF0lKiK36t6StMygBoL8FG75wAM',
+                        'https://drive.google.com/uc?export=view&id=1YVVJoKnnoR__JDjPFBR23cIgtNwOyKT2',
+                        'https://drive.google.com/uc?export=view&id=1b8Sh7zHb12wRJ8XLzGkXaPAkzq3nz-fQ',
+                        'https://drive.google.com/uc?export=view&id=1xo_sJ-7I-k24CPPmZq9q_3agFdA3h8Wh',
+                        'https://drive.google.com/uc?export=view&id=1-uq9qWqpuaXIROYZ2JpwD1yjtjrLHKYU',
+                        'https://drive.google.com/uc?export=view&id=1QGK0miNBlv4K169h7I5AoKaHrMUhe2D8',
+                        'https://drive.google.com/uc?export=view&id=1tGsV8TXK6dNC1bV_hkejO8cBAFEzC8hC',
+                        'https://drive.google.com/uc?export=view&id=1jmIhDmGPqEpSKC9sYN0TtJ98KByHPCLv',
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+                {
+                    title: 'Milkfish in Corn Oil',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=1InE4P0MTS6u94MtAV4icZh51bAn8ABKp',
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+                {
+                    title: 'Smoked Bangus (Marinated and Plain)',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=1UiGmss9RDlDSfr3lBGbISG96fNvQbscP',
                     ],
                     description:
                         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
@@ -29,13 +71,85 @@ export const PROJECT_COMPONENTS: ProjectComponent[] = [
         description:
             "From Seed to Harvest: Discover the Latest Trends and Best Practices in Aquaculture to Produce High-Quality Fish in a Sustainable, Profitable, and Environmentally Responsible Manner, Whether You're a Commercial Fish Farmer, Researcher, or Aquaculture Enthusiast.",
         image: 'https://drive.google.com/uc?export=download&id=1TCw5hRntiE0pKVKu17nawBx6mbEuxCDu',
+        detail: {
+            // videoTeaser:
+            //     'https://drive.google.com/uc?export=download&id=1Uz6R862hWGtPtTqM7gIt80IlWqJ_LhEA',
+            items: [
+                {
+                    title: 'Pond Preparation',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=19h_vRLglnDjVksBeRGavveMkG6_WuDfQ',
+                        
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+                {
+
+                    title: 'Stocking',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=1MTDCvtlPGFDBj2kAtKKr9KYcM3MZGGyQ',
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+                {
+                    title: 'Transfer of Stock ( from Nursery Pond to Trasition Pond',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=1InE4P0MTS6u94MtAV4icZh51bAn8ABKp',
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+            ],
+        },
     },
     {
         id: '3',
-        title: 'Museum',
+        title: 'Fisheries and Marine Science Virtual Museum',
         description:
             'Step into a World of Wonder and Discovery: Experience the Fascinating Intersection of History, Science, and Art Through Our Interactive Exhibits, Collections, and Programs, Designed to Educate, Engage, and Inspire Visitors of All Ages and Backgrounds.',
         image: 'https://drive.google.com/uc?export=download&id=1g1mJxHfJ2_DQoseZe6pj6P9mqN4xOX4N',
+        detail: {
+            // videoTeaser:
+            //     'https://drive.google.com/uc?export=download&id=1Uz6R862hWGtPtTqM7gIt80IlWqJ_LhEA',
+            items: [
+                {
+                    title: 'Boneless Bangus',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=19h_vRLglnDjVksBeRGavveMkG6_WuDfQ',
+                        
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+                {
+
+                    title: 'Boneless Kroepeck',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=1MTDCvtlPGFDBj2kAtKKr9KYcM3MZGGyQ',
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+                {
+                    title: 'Milkfish in Corn Oil',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=1InE4P0MTS6u94MtAV4icZh51bAn8ABKp',
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+                {
+                    title: 'Smoked Bangus (Marinated and Plain)',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=1UiGmss9RDlDSfr3lBGbISG96fNvQbscP',
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+            ],
+        },
     },
     {
         id: '4',
@@ -43,5 +157,46 @@ export const PROJECT_COMPONENTS: ProjectComponent[] = [
         description:
             'Experience the Rich Bounty of Nature at La Guerta: A Sustainable Farming Destination Where You Can Connect with the Land, Learn about Organic and Regenerative Agriculture, and Savor the Fresh, Flavorful Produce Grown Right Here on Our Farm. Join Us on a Journey from Seed to Harvest, and Discover the Joy and Nourishment of Farm-to-Plate Living.',
         image: 'https://drive.google.com/uc?export=download&id=1JjYJ8YFqOSoEzdVB0piYzc0HTBB6OzqG',
+        detail: {
+            // videoTeaser:
+            //     'https://drive.google.com/uc?export=download&id=1Uz6R862hWGtPtTqM7gIt80IlWqJ_LhEA',
+            items: [
+                {
+                    title: 'Boneless Bangus',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=19h_vRLglnDjVksBeRGavveMkG6_WuDfQ',
+                        
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+                {
+
+                    title: 'Boneless Kroepeck',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=1MTDCvtlPGFDBj2kAtKKr9KYcM3MZGGyQ',
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+                {
+                    title: 'Milkfish in Corn Oil',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=1InE4P0MTS6u94MtAV4icZh51bAn8ABKp',
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+                {
+                    title: 'Smoked Bangus (Marinated and Plain)',
+                    images: [
+                        'https://drive.google.com/uc?export=view&id=1UiGmss9RDlDSfr3lBGbISG96fNvQbscP',
+                    ],
+                    description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsa repellat sunt molestias rem odio repudiandae possimus totam iusto laudantium nostrum cumque suscipit assumenda, aliquam perferendis vero velit recusandae! Excepturi!',
+                },
+            ],
+            
+        },
     },
 ]
--- src/app/app-core/routes/project-components.routing.ts
+++ src/app/app-core/routes/project-components.routing.ts
@@ -2,6 +2,8 @@ import {Route} from '@angular/router'
 import {ProjectComponentDetailsComponent} from 'app/modules/landing/project-components/project-component-details/project-component-details.component'
 import {ProjectComponentsComponent} from 'app/modules/landing/project-components/project-components.component'
 
+
+
 export const PROJECT_COMPONENTS_ROUTING: Route[] = [
     {path: '', component: ProjectComponentsComponent},
     {path: ':id', component: ProjectComponentDetailsComponent},
