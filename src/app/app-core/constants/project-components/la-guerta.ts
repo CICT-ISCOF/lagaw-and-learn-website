9c6171538f5bd55d01531d1781a068ecd8d916f0
Edzel Buenavides <edzel0718@gmail.com>
2023-06-30 13:06:02 +0800

    changes

--- src/app/app-core/constants/project-components/la-guerta.ts
+++ src/app/app-core/constants/project-components/la-guerta.ts
@@ -9,7 +9,7 @@ export const LA_GUERTA = {
             {
                 id: '1',
                 title: 'La Guerta',
-                video: 'https://rr6---sn-4wg7ln7l.c.drive.google.com/videoplayback?expire=1685397318&ei=Bud0ZJqNB4-Jhwa5jrOABQ&ip=91.73.81.203&cp=QVRNV0RfVlNPSVhPOjVWSGptWjREOGwyUUhSUk9pTGxLV0lBUHppX3Bvdk9xNE5JeWoxOF9BQ0Q&id=f35691f37849e785&itag=22&source=webdrive&requiressl=yes&mh=95&mm=32&mn=sn-4wg7ln7l&ms=su&mv=m&mvi=6&pl=22&ttl=transient&susc=dr&driveid=1--SFW11tc_edusDR_H3-uDe1z416Vpda&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=295.775&lmt=1681352963765166&mt=1685382519&subapp=DRIVE_WEB_FILE_VIEWER&txp=0011224&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRAIgK2qowBjmfVjIbULiHVFGUohvOhE55YKbFhe6790S0FUCIHP_hdX2lmc46TglPR4hubQWcOSWG8Wl4xGbvmk-Vb6r&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRAIgUzd6e6kEHmM0OHAd-qAswhajhsAM6caUGJdEaLx_PZwCID06dwkbiqlBAQFHKjuNajwet9q5wPhAXRWnQ1UH_ylU&cpn=8z5q98xC3xFD-V_Y&c=WEB_EMBEDDED_PLAYER&cver=1.20230523.01.00',
+                video: 'https://drive.google.com/uc?id=1y5WcXiuyF_63aOlKihDd7hkjnd3B9N5U',
                 images: [
                     'https://lh5.googleusercontent.com/p/AF1QipMXCZ0NM5YyHX0I88gt2Sl5dRx0nr5o38KI_Eky=w1040-h350-n-k-no',
                 ],
